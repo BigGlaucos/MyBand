74ad75caf6780cffdb3a9b233988cd198080a0fa
Tony Rodrigues <88041342+TonyRodIv@users.noreply.github.com>
2023-04-12 17:33:48 -0300

    Melhorias e funcionabilidade!!

--- Tony/main.js
+++ Tony/main.js
@@ -1,4 +1,4 @@
 window.addEventListener('scroll', function() {
     var navbar = document.querySelector('.navbar');
-    navbar.classList.toggle('change', window.scrollY > 0);
+    navbar.classList.toggle('change', window.scrollY > 10);
   });
