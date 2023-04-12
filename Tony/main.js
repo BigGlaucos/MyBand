89e37f32de88e928fe6e208f580ad30727b1c8d7
Tony Rodrigues <88041342+TonyRodIv@users.noreply.github.com>
2023-04-12 17:55:58 -0300

    Melhorias visuais

--- Tony/main.js
+++ Tony/main.js
@@ -2,3 +2,17 @@ window.addEventListener('scroll', function() {
     var navbar = document.querySelector('.navbar');
     navbar.classList.toggle('change', window.scrollY > 10);
   });
+
+  const about = document.getElementById('about')
+  const fullText = document.getElementById('fullText')
+  const smallText = document.getElementById('smallText')
+function moreText(){
+  smallText.style.display = 'none'
+  fullText.style.display = 'block'
+  about.style.background = 'linear-gradient(180deg, rgba(12, 12, 12, 0.8) 0%, rgb(12, 12, 12) 90%)'
+}
+function lessText(){
+  smallText.style.display = 'block'
+  fullText.style.display = 'none'
+  about.style.background = 'linear-gradient(180deg, rgba(12, 12, 12, 0.5) 20%, rgb(12, 12, 12) 90%)'
+}
